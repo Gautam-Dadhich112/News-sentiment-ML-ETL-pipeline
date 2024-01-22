8067bceb313d762aa877c4876250b6fb7a546cb1
Gautam Dadhich <gautam.dadhich@episource.com>
2024-01-22 18:14:17 +0530

    Final Changes

--- News-sentiment-ML-ETL-pipeline/news_api.py
+++ News-sentiment-ML-ETL-pipeline/news_api.py
@@ -11,13 +11,14 @@ def news_fetch(topic):
   response.ok # for status code less than 400 it returns true
   response_json = response.json()
   response_data = response_json['articles']
+
   with open("articles.json", "w") as outfile:
     json.dump(response_data, outfile)
   extracted_df = pd.read_json('articles.json')
   extracted_df.drop(['source', 'author', 'urlToImage'], axis=1)
   tittle_extracted = extracted_df['title']
   content_extracted = extracted_df['content']
-  # print(len(tittle_extracted)) -- all the new tittle were less than 200 as len of this and tittle_for_analysis are same
+
   data_for_analysis = []
   for i in tittle_extracted:
       data_for_analysis.append(i)
@@ -28,6 +29,7 @@ def news_fetch(topic):
 
       data_for_analysis[j] = data_for_analysis[j] + " - " + i[:rem_len - 3]
       j += 1
+    
   newDf = pd.DataFrame(data_for_analysis, columns = ['Content'])
   newDf = newDf.drop(newDf[newDf['Content'] == '[Removed] - [Removed]'].index) # preprocessing the data
   newDf['Date'] = extracted_df['publishedAt']
@@ -35,22 +37,15 @@ def news_fetch(topic):
   newDf['url'] = extracted_df['url']
   sentiment_analysis(newDf)
   newDf = newDf.drop(['Subjectivity', 'Polarity'], axis=1)
-  # host="mysql"
-  # user="root"
-  # password="root"
-  # database="sentiment_analysis"
-  # port=3306
+
   def get_connection():
     return create_engine(
-      # url="mysql+mysqlconnector://root:@localhost/sentiment_analysis"
       url="mysql+mysqlconnector://root:root@mysql/sentiment_analysis"
     )
 
   engine = get_connection()
-  # engine = create_engine("mysql+mysqlconnector://root:root@localhost:3306/sentiment_analysis")
   newDf.drop_duplicates()
   newDf.to_sql('sentiments', con=engine, if_exists='replace', index=False)
-  newDf[newDf['Date'] == '2024-01-07']
 
 def sentiment_analysis(tweet):
  def getSubjectivity(text):
